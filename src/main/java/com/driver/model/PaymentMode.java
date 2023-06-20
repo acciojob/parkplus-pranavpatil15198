a848be17fb5c2536bfc6a1ec7627acce6335d77b
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-06-20 09:15:05 +0530

    Update

--- src/main/java/com/driver/model/PaymentMode.java
+++
@@ -1,6 +0,0 @@
-package com.driver.model;
-
-public enum PaymentMode {
-
-    CASH, CARD, UPI
-}
--- src/main/java/com/driver/model/SpotType.java
+++
@@ -1,6 +0,0 @@
-package com.driver.model;
-
-public enum SpotType {
-
-    TWO_WHEELER, FOUR_WHEELER, OTHERS
-}
